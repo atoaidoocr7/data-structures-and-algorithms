88800799ce16e874a965959a93dc03cd10120809
Ato Aidoo <77356776+atoaidoocr7@users.noreply.github.com>
2023-04-25 22:49:43 -0400

    Graph Class Changes

--- code/src/graphs/Graph.java
+++ code/src/graphs/Graph.java
@@ -1,9 +1,25 @@
 package graphs;
 
-import java.util.ArrayList;
 import java.util.List;
-
 public class Graph {
+    private List<Node> vertices;
 
+    public List<Node> getVertices() {
+        return vertices;
+    }
+    public void setVertices(List<Node> vertices) {
+        this.vertices = vertices;
+    }
+    private List<Node> breadthFirstSearch(Node source){
+        return null;
+    }
+    private List<Node> depthFirstSearch(Node source){
+        return null;
+    }
+    private void breadthFirstSearch(Node source, List<Node> output){
 
+    }
+    private void depthFirstSearch(Node source, List<Node> output){
+
+    }
 }
